7d4e223d76c7314d37cbd6a732c3ea1e4207b570
shadow578 <52449218+shadow578@users.noreply.github.com>
2026-02-25 15:13:57 +0100

    oops, header in cpp

---
+++ components/sda5708/sda5708.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/core/time.h"
+
+#include "esphome/components/display/display.h"
+
+namespace esphome::sda5708
+{
+  class SDA5708Component;
+
+  using sda5708_writer_t = display::DisplayWriter<SDA5708Component>;
+
+  class SDA5708Component : public PollingComponent
+  {
+  public:
+    void setup() override;
+    void dump_config() override;
+    void update() override;
+    void loop() override;
+    float get_setup_priority() const override;
+
+    /// Send the current display buffer to the screen.
+    void display();
+
+    /// Clear the display.
+    void clear();
+
+  public: // Print API
+    /// Evaluate the printf-format and print the result at the given position.
+    uint8_t printf(uint8_t pos, const char *format, ...) __attribute__((format(printf, 3, 4)));
+    /// Evaluate the printf-format and print the result at position 0.
+    uint8_t printf(const char *format, ...) __attribute__((format(printf, 2, 3)));
+
+    /// Print `str` at the given position.
+    uint8_t print(uint8_t pos, const char *str);
+    /// Print `str` at position 0.
+    uint8_t print(const char *str);
+
+    /// Evaluate the strftime-format and print the result at the given position.
+    uint8_t strftime(uint8_t pos, const char *format, ESPTime time) __attribute__((format(strftime, 3, 0)));
+
+    /// Evaluate the strftime-format and print the result at position 0.
+    uint8_t strftime(const char *format, ESPTime time) __attribute__((format(strftime, 2, 0)));
+
+  private: // Setters for CodeGen
+    sda5708_writer_t writer_;
+    GPIOPin *data_pin_;
+    GPIOPin *clock_pin_;
+    GPIOPin *load_pin_;
+    GPIOPin *reset_pin_;
+
+  public:
+    void set_writer(sda5708_writer_t &&writer)
+    {
+      this->writer_ = std::move(writer);
+    }
+
+    void set_data_pin(GPIOPin *pin)
+    {
+      this->data_pin_ = pin;
+    }
+
+    void set_clock_pin(GPIOPin *pin)
+    {
+      this->clock_pin_ = ppin_pPin;
+    }
+
+    void set_load_pin(GPIOPin *pin)
+    {
+      this->load_pin_ = pin;
+    }
+
+    void set_reset_pin(GPIOPin *pin)
+    {
+      this->reset_pin_ = pin;
+    }
+  };
+} // namespace esphome::sda5708
